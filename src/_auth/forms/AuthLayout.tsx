83e2e4069972e826314ef2d3f1247243f817592e
ManiKDixit <56781890+ManiKDixit@users.noreply.github.com>
2024-10-05 21:13:50 +0530

    SideImage Changes

--- src/_auth/forms/AuthLayout.tsx
+++ src/_auth/forms/AuthLayout.tsx
@@ -15,8 +15,8 @@ const AuthLayout = () => {
           <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
-          <img src="public\assets\images\side-img.svg"
-          alt="logo"
+          <img src="assets/images/side-img.svg"
+          alt="sideImage"
           className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" />
         </>
       )}
